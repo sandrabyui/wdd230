322220c00f27e4467b1cb59837ee77a7740fb271
sandrabyui <ade21006@byui.edu>
2022-11-12 23:53:49 +0100

    json for the directory

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -18,11 +18,11 @@ fetch(requestURL)
     let portrait = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
+    h2.textContent = prophet.name + ' ' + prophet.lastname +' / '+ prophet.birthdate +'/ '+prophet.birthplace;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname +' '+ prophet.birthdate +' '+ prophet.birthplace );
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element

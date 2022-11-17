936a1477c9e3489b433fcc6cc280f894bd2cbc7e
sandrabyui <ade21006@byui.edu>
2022-11-17 14:30:00 +0100

    updates and lesson9,weather

--- Chamber_home/bussinessDirectory/data.js
+++ Chamber_home/bussinessDirectory/data.js
@@ -1,4 +1,4 @@
-const requestURL ="https://github.com/sandrabyui/wdd230/commit/322220c00f27e4467b1cb59837ee77a7740fb271";
+const requestURL ="./data.json";
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector("div");
@@ -12,14 +12,23 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const companies = jsonObject['companies'];
-    companies.forEach(displaycompanies);
+    companies.forEach(displayCompanies);
   });
   //const prophets = jsonObject['prophets'];
   //companies.forEach(displaycompanies);
-  
-  
+
+  function displayCompanies(companies) {
+
+    let card=document.createElement("section");
+    let companiesName = document.createElement("h2");
+    let companiesAdd = document.createElement("p");
+    let companiesPhone =document.createElement("p");
+    let companiesImg = document.createElement("img");
     // Change the textContent property of the h2 element to contain the companies full name
-    h2.textContent = companies.name + ' ' + companies.address +' / '+ companies.phone;
+    companiesName.textContent =`${companies.name}`; 
+    companiesAdd.textContent =`${companies.address}`;
+    companiesPhone.textContent =`${companies.phone}`;
+    companiesImg.textContent =`${companies.imageurl}`;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     grid.setAttribute('src', companies.imageurl);
@@ -27,11 +36,15 @@ fetch(requestURL)
     grid.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(grid);
+    card.appendChild(companiesName);
+    card.appendChild(companiesAdd);
+    card.appendChild(companiesPhone);
+    card.appendChild(companiesImg);
+    
+    
   
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.menu').appendChild(menu);
+    document.querySelector('div.menu').appendChild(card);
 
 
 // The following code could be written cleaner. How We may have to simplfiy our HTMl and think about a default view.
@@ -44,7 +57,9 @@ gridbutton.addEventListener("click", () => {
 
 listbutton.addEventListener("click", showList); // example using defined function
 
+
 function showList() {
 	display.classList.add("list");
 	display.classList.remove("grid");
+}
 }
---
+++ lesson10/weatherapi.js
@@ -0,0 +1,17 @@
+const apiURL = "//api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=1aa89f37d116175977bfb29a44e97e60";
+
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
